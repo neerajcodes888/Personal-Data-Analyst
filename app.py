94fb5b3ff26af0e39fbf9601b7bf60f3d02fac42
neeraj <neerajmail888@gmail.com>
2024-03-15 15:14:10 +0530

    cccc

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def save_image(base64_str, save_path):
 lida = Manager(text_gen = llm("openai")) 
 textgen_config = TextGenerationConfig(n=1, temperature=0.2, use_cache=True)
 
-menu = st.sidebar("Analyze Your CSv data")
+st.sidebar.header("Analyze Your CSV")
 
